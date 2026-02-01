4d4ff4052599d5aa1deb1cf3d14d83f2a13e56d3
olumild <olumild998@gmail.com>
2026-02-01 22:46:30 +0100

    fourth commit

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://localhost:7000/api" })
+const baseUrl = import.meta.env.MODE === "development" ? "http://localhost:7000/api" : "/api"
+
+const api = axios.create({ baseUrl })
 
 export default api
